ad7e7c4609b792072b93c3b4b02b5e79ab766d12
root <root@22-12-4-2234.vcn.oraclevcn.com>
2026-08-17 17:01:22 +0800

    refactor: 移除前端在线更新界面
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- web/src/components/modules/setting/index.tsx
+++ web/src/components/modules/setting/index.tsx
@@ -6,7 +6,6 @@ import { SettingSystem } from './System';
 import { SettingAPIKey } from './APIKey';
 import { SettingLLMPrice } from './LLMPrice';
 import { SettingAccount } from './Account';
-import { SettingInfo } from './Info';
 import { SettingLLMSync } from './LLMSync';
 import { SettingLog } from './Log';
 import { SettingBackup } from './Backup';
@@ -16,7 +15,6 @@ export function Setting() {
     return (
         <div className="h-full min-h-0 overflow-y-auto overscroll-contain rounded-t-3xl">
             <PageWrapper className="columns-1 gap-4 pb-24 md:columns-2 md:pb-4 *:mb-4 *:break-inside-avoid">
-                <SettingInfo key="setting-info" />
                 <SettingAppearance key="setting-appearance" />
                 <SettingAccount key="setting-account" />
                 <SettingSystem key="setting-system" />
